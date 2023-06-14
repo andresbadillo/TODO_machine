8138872cc056b9572f22d10d7e8b3b1766b646b7
Andres Badillo <r.andres.badillo@gmail.com>
2023-06-14 15:15:43 -0500

    Preparacion 2 - Inicio curso React Routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,102 +1,20 @@
 import React from 'react';
-import { useTodos } from './useTodos';
-import { TodoCounter } from '../ui/TodoCounter';
-import { TodoSearch } from '../ui/TodoSearch';
-import { TodoList } from '../ui/TodoList';
-import { TodoItem } from '../ui/TodoItem';
-import { TodosLoading } from '../ui/TodosLoading';
-import { TodosError } from '../ui/TodosError';
-import { TodosEmpty } from '../ui/TodosEmpty';
-import { CreateTodoButton } from '../ui/CreateTodoButton';
-import { TodoForm } from '../ui/TodoForm';
-import { Modal } from '../ui/Modal';
-import { TodoHeader } from '../ui/TodoHeader';
-import { ChangeAlert } from '../ui/ChangeALert';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './home/HomePage';
+import { NewTodoPage } from './new/NewTodoPage';
+import { EditTodoPage } from './edit/EditTodoPage';
 
 function App() {
-  const { state, stateUpdaters } = useTodos();
-
-  const {
-    error,
-    loading,
-    searchedTodos,
-    totalTodos,
-    completedTodos,
-    openModal,
-    searchValue,
-  } = state;
-  
-  const {
-    setOpenModal,
-    addTodo,
-    completeTodo,
-    deleteTodo,
-    setSearchValue,
-    sincronizeTodos,
-  } = stateUpdaters;
 
   return (
-    <React.Fragment>
-
-      <TodoHeader loading={loading}>
-        <TodoCounter 
-          totalTodos={totalTodos} 
-          completedTodos={completedTodos}
-        />
-        <TodoSearch 
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-        />
-      </TodoHeader>
-
-      <TodoList
-        error={error}
-        loading={loading}
-        searchedTodos={searchedTodos}
-        totalTodos={totalTodos}
-        searchText={searchValue}
-        onError={() => <TodosError />}
-        onLoading={() => <TodosLoading />}
-        onEmptyTodos={() => <TodosEmpty />}
-        onEmptySearchResults={(searchText) => <p>No hay resultados para tu búsqueda de {searchText}</p>}
-        render={todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        )}
-      >
-        {/* {todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        )} */}
-      </TodoList>
-
-      <CreateTodoButton setOpenModal={setOpenModal}/>
-
-      {openModal && (
-        <Modal>
-          <TodoForm 
-            addTodo={addTodo}
-            setOpenModal={setOpenModal}
-          />
-        </Modal>
-      )}
-
-      <ChangeAlert 
-        sincronize={sincronizeTodos}
-      />
-
-    </React.Fragment>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/new' element={<NewTodoPage />} />
+        <Route path='/edit/:id' element={<EditTodoPage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
-export default App;
+export { App };
